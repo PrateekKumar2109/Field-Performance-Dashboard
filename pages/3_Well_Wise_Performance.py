ab38f90a0dd297c72df4213588b0ac26bba28d3d
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2025-04-10 14:57:54 +0530

    Update 3_Well_Wise_Performance.py

--- pages/3_Well_Wise_Performance.py
+++ pages/3_Well_Wise_Performance.py
@@ -15,7 +15,7 @@ if data_uploader is not None:
           data_df=data_df[['Platform','Well No','Date','Days','YEAR','Ql, blpd', 'Qo, bopd', 'Qw, bopd','RecOil, bbls   ',
                   'Qg (Assoc. Gas), m3/d','Moil, MMt', 'RecGas, m3']]  
     except:      
-          data_df=pd.read_excel(data_uploader,engine='pyxlsb' )
+          data_df=pd.read_excel(data_uploader)
 
           data_df=data_df[['Platform','Well No','Date','Days','YEAR','Ql, blpd', 'Qo, bopd', 'Qw, bopd','RecOil, bbls   ',
                   'Qg (Assoc. Gas), m3/d','Moil, MMt', 'RecGas, m3']]
